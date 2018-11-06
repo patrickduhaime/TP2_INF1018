ab9f60863a811f5277d14c60fc4b9bb80c5db399
patrickduhaime <32279890+patrickduhaime@users.noreply.github.com>
2018-11-05 20:34:54 -0500

    ajout a AnalSyn - methodeErreur

--- TP2_AnalyseProgramme_HBrochu_PDuhaime/AnalSyn.cs
+++ TP2_AnalyseProgramme_HBrochu_PDuhaime/AnalSyn.cs
@@ -21,11 +21,11 @@ namespace TP2_AnalyseProgramme_HBrochu_PDuhaime
         public bool Analyse()
         {
             if (!AnalyseProcedure())
-                return false;
+                return methodeErreur(1);
 
             while (lexemes[0] == "declare")
                 if (!AnalyseDeclarations())
-                    return false;
+                    return methodeErreur(2);
 
             while (lexemes.Count != 0)
                 if (!AnalyseInstructions())
@@ -38,7 +38,7 @@ namespace TP2_AnalyseProgramme_HBrochu_PDuhaime
         {
             if (lexemes[0] != "Procedure" || !lexemes[1].StartsWith("ID") || lexemes[lexemes.Count - 2] != "Fin_Procedure" 
                 || !lexemes[lexemes.Count - 1].StartsWith("ID"))
-                return false;
+                 return false;
 
             Procedure.Add(lexemes[0]);
             Procedure.Add(lexemes[1]);
@@ -92,33 +92,70 @@ namespace TP2_AnalyseProgramme_HBrochu_PDuhaime
             //TODO: Vérification de la validité des opérations mathématiques
 
             if (!instruction[0].StartsWith("ID"))
-                return false;
+                return methodeErreur(3);
 
             if (instruction[1] != "=")
-                return false;
+                return methodeErreur(4);
 
             if (instruction[2] != "Number")
-                return false;
+                return methodeErreur(5);
 
             if (instruction[instruction.Count-1] != "Number")
-                return false;
+                return methodeErreur(6);
 
             for (int i = 4; i < instruction.Count; i+=2)
             {
                 if (instruction[i] != "Number")
-                    return false;
+                    return methodeErreur(7);
                 if (instruction[i - 1] != "+" && instruction[i - 1] != "-" && instruction[i - 1] != "*" && instruction[i - 1] != "/")
-                    return false;
+                    return methodeErreur(8);
             }
-
 
             Instructions.Add(instruction);
 
             for (int i = 0; i < instruction.Count; i++)
                 lexemes.RemoveAt(0);
 
-            return true;
+            return true;  
         }
+        
 
+        private bool methodeErreur(int code)
+        {
+            ///////Traitement de l’erreur
+
+            switch (code)
+            {
+                case 1:
+                    Console.WriteLine("La syntaxe de la procedure est erronée ! Analyse syntaxique Erreur 01");
+                    break;
+                case 2:
+                    Console.WriteLine("La syntaxe de la declaration est erronée ! Analyse syntaxique Erreur 02");
+                    break;
+                case 3:
+                    Console.WriteLine("Une instruction doit debuter par un identificateur ! Analyse syntaxique Erreur 03");
+                    break;
+                case 4:
+                    Console.WriteLine("Une instruction doit avoir un signe '=' apres l'identificateur ! Analyse syntaxique Erreur 04");
+                    break;
+                case 5:
+                    Console.WriteLine("Une instruction doit avoir un nombre apres le signe '=' ! Analyse syntaxique Erreur 05");
+                    break;
+                case 6:
+                    Console.WriteLine("Une instruction doit se terminer par un nombre ! Analyse syntaxique Erreur 06");
+                    break;
+                case 7:
+                    Console.WriteLine("Une instruction doit contenir des nombres seulement ! Analyse syntaxique Erreur 07");
+                    break;
+                case 8:
+                    Console.WriteLine("Une instruction doit contenir les operateurs mathematiques + - * ou / seulement ! Analyse syntaxique Erreur 08");
+                    break;
+                default:
+                    Console.WriteLine("Analyse syntaxique Erreur");
+                    break;
+            }
+
+            return false;
+        }
     }
 }
